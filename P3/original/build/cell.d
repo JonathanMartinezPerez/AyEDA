5562275890fdb082198d59f5b5b72634404f62ee
Jonathan Martinez <alu0101254098@ull.edu.es>
2024-03-17 13:31:07 +0100

    Eliminando basura

--- P3/original/build/cell.d
+++
@@ -1,6 +0,0 @@
-build/cell.o: src/cell.cc src/../include/cell.h src/../include/lattice.h \
- src/../include/factorycell.h src/../include/position.h
-src/../include/cell.h:
-src/../include/lattice.h:
-src/../include/factorycell.h:
-src/../include/position.h:
--- P3/original/build/factorycell.d
+++
@@ -1,6 +0,0 @@
-build/factorycell.o: src/factorycell.cc src/../include/factorycell.h \
- src/../include/cell.h src/../include/lattice.h src/../include/position.h
-src/../include/factorycell.h:
-src/../include/cell.h:
-src/../include/lattice.h:
-src/../include/position.h:

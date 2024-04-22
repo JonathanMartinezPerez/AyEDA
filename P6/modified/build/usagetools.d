f4c3a127c886ae7eeedb99afbe10bd0afd7e6940
Jonathan Martinez <alu0101254098@ull.edu.es>
2024-04-22 10:16:04 +0200

    end

--- P6/modified/build/usagetools.d
+++
@@ -1,8 +0,0 @@
-build/usagetools.o: src/usagetools.cc include/usagetools.h include/nif.h \
- include/AB.h include/nodoB.h include/ABB.h include/ABE.h
-include/usagetools.h:
-include/nif.h:
-include/AB.h:
-include/nodoB.h:
-include/ABB.h:
-include/ABE.h:
